8061052288d5af273b1e4127d9e40c450cce124c
Adam Nalgiev <adamnalgiev006@gmail.com>
2025-07-07 13:08:39 +0300

    Task ready

--- app/src/main/java/ru/myapp/myapplication/MainActivity.kt
+++ app/src/main/java/ru/myapp/myapplication/MainActivity.kt
@@ -1,13 +1,13 @@
 package ru.myapp.myapplication
 
 import android.os.Bundle
-import android.os.SystemClock
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,23 +19,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
 
-    override fun onResume() {
-        super.onResume()
-        val currentTime = SystemClock.uptimeMillis()
-        calculateLaunchingTime(currentTime)
-    }
-
-    private fun calculateLaunchingTime(appearedTime: Long) {
-        val uri = "content://ru.myapp.myapplication.launch-provider".toUri()
-
-        contentResolver.query(uri, null, null, null)?.use { cursor ->
-            if (cursor.moveToFirst()) {
-                val appStartTime = cursor.getLong(0)
-                val launchTime: Long by KDelegate(appStartTime, appearedTime)
-                Log.d("TIME!", launchTime.toString())
-            }
+        lifecycleScope.launch(Dispatchers.IO) {
+            Flows().collectFlow()
         }
     }
+
 }
